646bb385129320a7609dba9264a8adabd5dba467
gksqlc6401 <gksqlc6401@naver.com>
2021-07-09 19:05:15 +0900

    .

--- src/mini_project11/MM.java
+++ src/mini_project11/MM.java
@@ -1,8 +1,9 @@
 package mini_project11;
 
-public class Mini {
+public class MM {
 
   public static void main(String[] args) {
-    System.out.println("mini");
+    System.out.println("mmmms");
   }
+
 }
