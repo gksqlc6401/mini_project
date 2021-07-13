269833267a1e80effcc841651394abe9070791d3
gksqlc6401 <gksqlc6401@naver.com>
2021-07-13 09:33:27 +0900

    .

--- src/mini_project11/Cgv.java
+++ src/mini_project11/Cgv.java
@@ -32,7 +32,7 @@ public class Cgv {
     Cgv a = new Cgv();
     try { 
       a.dbConnect();
-      a.dbSelect(); 
+      // a.dbSelect(); 
       a.getTitle(); 
       a.getDate();
       a.getTime();
@@ -103,67 +103,78 @@ public class Cgv {
       /*ST=CN.createStatement();
       RS=ST.executeQuery(msg);
       msg="insert into"*/
-      System.out.println("오라클 드라이브및 서버연결성공");
       System.out.println();
-      System.out.println("1.괴물 2.어벤져스:엔드게임 3.극한직업");
-      System.out.println("상영하실 영화를 입력하세요: ");
+      System.out.println("1.괴물     2.어벤져스:엔드게임     3.극한직업\n");
+      System.out.print("상영하실 영화를 입력하세요: ");
       title =sc.nextLine();
+      System.out.println();
       System.out.println( title +" 선택하셨습니다");
     }catch(Exception ex){System.out.println("error =" + ex);}
   }
 
   public void getDate() {
     try {
+      System.out.println();
       System.out.print("19일 ~ 25일 중에 입력해 주세요(예: 19)>> ");
-      Scanner sc = new Scanner(System.in);
       date = sc.nextLine();
+      System.out.println();
       System.out.println("7월 "+date+"일로 예약 완료");
     } catch(Exception ex) { System.out.println("error = " + ex);} 
   }
 
-  void getTime(){
+  public void getTime() {
 
     Scanner sc = new Scanner(System.in);
+    loop: while(true) {
+      try {
+        System.out.println("[시간대 선택 - 조조 / 일반 / 심야 ] ");
+        System.out.println("조조 할인 시간 (10,000원) : [1] AM  8:00 - 10:00 \t[2] AM 10:00 - 12:00 \n");
+        System.out.println("일반 상영 시간 (14,000원) : [3] PM  2:00 -  4:00 \t[4] PM  4:00 -  6:00 \n");    
+        System.out.println("심야 상영 시간 (12,000원) : [5] PM  8:00 - 10:00 \t[6] PM 10:00 - 12:00 \n");
+        System.out.println("시간을 선택해주세요. 1 ~ 6 ");
+        System.out.println();
 
-    System.out.println("[시간대 선택 - 조조 / 일반 / 심야 ] ");
-    System.out.println("조조 할인 시간 (10,000원) : [1] AM  8:00 - 10:00 [2] AM 10:00 - 12:00 ");
-    System.out.println("일반 상영 시간 (14,000원) : [3] PM  2:00 -  4:00 [4] PM  4:00 -  6:00 ");    
-    System.out.println("심야 상영 시간 (12,000원) : [5] PM  8:00 - 10:00 [6] PM 10:00 - 12:00 ");
-    System.out.println("시간을 선택해주세요. 1 ~ 6 ");
+        int timeSelect = Integer.parseInt(sc.nextLine());
+        switch (timeSelect) {
+          case 1:
+            time = "AM 8:00 - 10:00";
+            ticketPrice = 10000;
+            break;
+          case 2:
+            time = "PM 10:00 - 12:00";
+            ticketPrice = 10000;
+            break;
+          case 3:
+            time = "PM 2:00- 4:00";
+            ticketPrice = 14000;
+            break;
+          case 4:
+            time = "PM 4:00 - 6:00";
+            ticketPrice = 14000;
+            break;
+          case 5:
+            time = "PM 8:00 - 10:00";
+            ticketPrice = 12000;
+            break;
+          case 6:  
+            time = "PM 10:00 - 12:00";
+            ticketPrice = 12000;
+            break;
+          default: System.out.println("올바른 시간을 선택해주세요.\n"); continue;
 
-    int timeSelect = Integer.parseInt(sc.nextLine());
-
-    switch(timeSelect) {
-      case 1:
-        time = "AM 8:00 - 10:00";
-        ticketPrice = 10000;
-        break;
-      case 2:
-        time = "PM 10:00 - 12:00";
-        ticketPrice = 10000;
-        break;
-      case 3:
-        time = "PM 2:00- 4:00";
-        ticketPrice = 14000;
-        break;
-      case 4:
-        time = "PM 4:00 - 6:00";
-        ticketPrice = 14000;
-        break;
-      case 5:
-        time = "PM 8:00 - 10:00";
-        ticketPrice = 12000;
-        break;
-      case 6:  
-        time = "PM 10:00 - 12:00";
-        ticketPrice = 12000;
-        break;
-      default:
-        System.out.println("올바른 시간을 선택해주세요.");
-        break;
-    }
-    System.out.println("선택한 시간이 맞습니까? Y / N ");
-    System.out.println("시간 : "+time+" , 티켓비용 : "+ticketPrice);
+        }
+      }catch (Exception e) {}
+      System.out.println("선택한 시간이 맞습니까? Y /n ");
+      String ans = sc.nextLine();
+      if (ans.equals("Y")) {
+        System.out.println("시간 : "+time+" , 티켓비용 : "+ticketPrice);
+        System.out.println();
+        return;
+      }else {
+        System.out.println("다시 선택해주세요");
+        continue;
+      }
+    }  
   }
 
   public void getSeat() {
@@ -199,17 +210,16 @@ public class Cgv {
       }else {
         System.out.println("이미 예약된 객실입니다");
       }
-      System.out.println("\n\t\t\t\t\t\t\t\t\t\t[ S C R E E N ]");
+      System.out.println("\n\t\t\t\t\t\t\t\t\t[ S C R E E N ]");
 
       String a= "ABCDEF";
       for(int i = 0; i < 6; i++){
         for(int j = 0; j < 10; j++){
           if(name[i][j] == null) {
 
-
-            System.out.print(" " + (a.charAt(i))+(j+1) +"좌석"+"□\t" +"\t"); 
+            System.out.print(" " + (a.charAt(i))+(j+1) +"좌석"+"□\t"); 
           }else {
-            System.out.print( " " + (a.charAt(i))+(j+1) +"좌석"+"■\t" + name[i][j] + "\t"); 
+            System.out.print( " " + (a.charAt(i))+(j+1) +"좌석"+"■ " + name[i][j]+"\t"); 
           }
         } //j end
         System.out.println();
