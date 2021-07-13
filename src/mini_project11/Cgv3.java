d8b5865c19d1e06469a388f269c1e3a09333eb0f
gksqlc6401 <gksqlc6401@naver.com>
2021-07-13 17:04:46 +0900

    .

--- src/mini_project11/Cgv3.java
+++ src/mini_project11/Cgv3.java
@@ -1,5 +1,4 @@
 package mini_project11;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,14 +8,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-public class CgvHandler extends ArrayList{
-
+public class Cgv3 {
   Connection CN=null; //DB서버연결정보 서버ip주소 계정id,pwd
   Statement ST=null;  //ST=CN.createStatement()명령어생성 삭제,신규등록,조회하라
   ResultSet RS=null;  //select조회결과값 전체데이터를 기억합니다
   String msg="";
   int Gtotal = 0;  
-  Scanner sc;
+
 
   public int floor; //층=행 3층
   public int room; 
@@ -27,6 +25,56 @@ public class CgvHandler extends ArrayList{
   int ticketPrice;
   String seat;
   String payDate;
+  public int atitle;
+  public int adate;
+  public int atime;
+  public String[][][] acinema = new String[3][7][6];
+  static Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    System.out.println("\t\t\tC G V");
+    System.out.println("\t\t영화 예매 프로그램"); 
+    System.out.println();
+
+    Cgv3 a = new Cgv3();
+    try { 
+      loop: while(true) {
+        System.out.print("[1.예매]   [2.전체 예매현황]   [9.종료]");
+        String sel = "9";
+        sel = sc.nextLine();
+        a.dbConnect();
+        a.arr();
+        //a.screen();
+
+        switch (sel) {
+          case "1":
+            a.getTitle(); 
+            a.getDate();
+            a.getTime();
+            a.getSeat();
+            a.pay(); break;
+          case "2":
+            a.dbSelect(); break;
+          case "9": System.out.println("영화표 예매 프로그램 종료하겠습니다."); break loop;
+          default : System.out.println("잘못입력하셨습니다."); continue;
+        }
+      }
+    }catch(Exception ex){ }
+  }
+
+  public void arr() {
+    acinema[atitle][adate][atime] = acinema[i][j][k];
+    for(int i=0; i<3; i++) {
+      for(int j=0; j<7; j++) {
+        for(int k=0; k<6; k++) {
+          acinema[i][j][k]="0";
+        }
+      }
+    }
+
+  }
+
   public void dbConnect() {
     try {
       Class.forName("oracle.jdbc.driver.OracleDriver"); //오라클드라이브로드
@@ -34,10 +82,9 @@ public class CgvHandler extends ArrayList{
     }catch(Exception ex){System.out.println("error =" + ex);}
   }//end
 
-
   public void dbSelect() {
     try {
-      System.out.println("전체 데이터출력중입니다.");
+      System.out.println("112233전체 데이터출력중입니다.");
       Thread.sleep(500);
       ST = CN.createStatement();
       msg ="select * from cinema";
@@ -63,10 +110,11 @@ public class CgvHandler extends ArrayList{
         System.out.print("상영하실 영화번호를 입력하세요: ");
         title =sc.nextLine();
         System.out.println();
+
         switch(title) {
-          case "1": title="괴물"; break;
-          case "2": title="어벤져스:엔드게임"; break;
-          case "3": title="극한직업"; break;
+          case "1": title="괴물"; atitle=0; break;
+          case "2": title="어벤져스:엔드게임"; atitle=1; break;
+          case "3": title="극한직업"; atitle=2; break;
           default : System.out.println("다시선택해주세요"); continue;
         }
         System.out.println();
@@ -83,13 +131,13 @@ public class CgvHandler extends ArrayList{
         System.out.print("19일 ~ 25일 중에 입력해 주세요(예: 19)>> \n");
         date = sc.nextLine();
         switch(date) {
-          case "19": date="19일"; break;
-          case "20": date="20일"; break;
-          case "21": date="21일"; break;
-          case "22": date="22일"; break;
-          case "23": date="23일"; break;
-          case "24": date="24일"; break;
-          case "25": date="25일"; break;
+          case "19": date="19일"; adate=0; break;
+          case "20": date="20일"; adate=1; break;
+          case "21": date="21일"; adate=2; break;
+          case "22": date="22일"; adate=3; break;
+          case "23": date="23일"; adate=4; break;
+          case "24": date="24일"; adate=5; break;
+          case "25": date="25일"; adate=6; break;
           default: System.out.println("다시입력해주세요\n"); continue;
         }
         System.out.println();
@@ -101,7 +149,8 @@ public class CgvHandler extends ArrayList{
 
   public void getTime() {
 
-    while(true) {
+    Scanner sc = new Scanner(System.in);
+    loop: while(true) {
       try {
         System.out.println("[시간대 선택 - 조조 / 일반 / 심야 ] ");
         System.out.println("조조 할인 시간 (10,000원) : [1] AM  8:00 - 10:00 \t[2] AM 10:00 - 12:00 \n");
@@ -114,27 +163,27 @@ public class CgvHandler extends ArrayList{
         switch (timeSelect) {
           case "1":
             time = "AM 8:00 - 10:00";
-            ticketPrice = 10000;
+            ticketPrice = 10000; atime=0;
             break;
           case "2":
             time = "PM 10:00 - 12:00";
-            ticketPrice = 10000;
+            ticketPrice = 10000; atime=1;
             break;
           case "3":
             time = "PM 2:00- 4:00";
-            ticketPrice = 14000;
+            ticketPrice = 14000; atime=2;
             break;
           case "4":
             time = "PM 4:00 - 6:00";
-            ticketPrice = 14000;
+            ticketPrice = 14000; atime=3;
             break;
           case "5":
             time = "PM 8:00 - 10:00";
-            ticketPrice = 12000;
+            ticketPrice = 12000; atime=4;
             break;
           case "6":  
             time = "PM 10:00 - 12:00";
-            ticketPrice = 12000;
+            ticketPrice = 12000; atime=5;
             break;
           default: System.out.println("올바른 시간을 선택해주세요.\n"); continue;
 
@@ -156,55 +205,54 @@ public class CgvHandler extends ArrayList{
   public void getSeat() {
 
     try {
-      while (true) {
-        System.out.print("무슨열 선택하시겠습니까?(A~F층까지)>>> ");
-        String row = sc.nextLine();
+      System.out.print("무슨열 선택하시겠습니까?(A~F층까지)>>> ");
+      String row = sc.nextLine();
 
-        switch(row) {
-          case "A" : floor=1; break;
-          case "B" : floor=2; break;
-          case "C" : floor=3; break;
-          case "D" : floor=4; break;
-          case "E" : floor=5; break;
-          case "F" : floor=6; break;
-          default : System.out.println("다시입력해주세요"); 
-        }
-        if(floor <1  || floor >6) {
-          System.out.println("해당 열은 존재하지 않습니다");
-        }
-
-        System.out.print("몇 번을 선택 하시겠습니까?(1~10번까지)>>> ");
-        room = Integer.parseInt(sc.nextLine());
-        if(room < 1 || room >10) {
-          System.out.println("해당 번호는 존재하지 않습니다"); continue;
-        }
-
-        if(name[floor-1][room-1] == null) {
-          System.out.print("이름을 입력하세요>>> ");
-          name[floor-1][room-1] = sc.nextLine();
-
-          System.out.println("좌석 예약 완료");
-        }else {
-          System.out.println("이미 예약된 좌석입니다");
-        }
-        System.out.println("\n\t\t\t\t\t\t\t\t\t[ S C R E E N ]");
-
-        String a= "ABCDEF";
-        for(int i = 0; i < 6; i++){
-          for(int j = 0; j < 10; j++){
-            if(name[i][j] == null) {
-
-              System.out.print(" " + (a.charAt(i))+(j+1) +"좌석"+"□\t"); 
-            }else {
-              System.out.print( " " + (a.charAt(i))+(j+1) +"좌석"+"■ " + name[i][j]+"\t"); 
-            }
-          } //j end
-          System.out.println();
-        }//for i end
-        seat=row+Integer.toString(room);
+      switch(row) {
+        case "A" : floor=1; break;
+        case "B" : floor=2; break;
+        case "C" : floor=3; break;
+        case "D" : floor=4; break;
+        case "E" : floor=5; break;
+        case "F" : floor=6; break;
+        default : System.out.println("다시입력해주세요"); break;
       }
+      if(floor <1  || floor >6) {
+        System.out.println("해당 열은 존재하지 않습니다");
+      }
+
+      System.out.print("몇 번을 선택 하시겠습니까?(1~10번까지)>>> ");
+      room = Integer.parseInt(sc.nextLine());
+      if(room < 1 || room >10) {
+        System.out.println("해당 번호는 존재하지 않습니다");
+      }
+
+      if(name[floor-1][room-1] == null) {
+        System.out.print("이름을 입력하세요>>> ");
+        name[floor-1][room-1] = sc.nextLine();
+
+        System.out.println("객실 예약 완료");
+      }else {
+        System.out.println("이미 예약된 객실입니다");
+      }
+      System.out.println("\n\t\t\t\t\t\t\t\t\t[ S C R E E N ]");
+
+      String a= "ABCDEF";
+      for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 10; j++){
+          if(name[i][j] == null) {
+
+            System.out.print(" " + (a.charAt(i))+(j+1) +"좌석"+"□\t"); 
+          }else {
+            System.out.print( " " + (a.charAt(i))+(j+1) +"좌석"+"■ " + name[i][j]+"\t"); 
+          }
+        } //j end
+        System.out.println();
+      }//for i end
+      seat=row+Integer.toString(room);
     }catch (Exception e) {System.out.println("에러이유:"+ e);}//end
   }
+
   public void pay() {
 
     try {
@@ -240,7 +288,7 @@ public class CgvHandler extends ArrayList{
 
           msg="insert into cinema values('"+title+"','"+date+"','"+time+"',"+ticketPrice+",'"+seat+"','"+payDate+"')";
           ST=CN.createStatement();
-          ST.executeUpdate(msg);
+          int rs = ST.executeUpdate(msg);
           return;
         }else if(ans.equals("N")){
           System.out.println("처음으로 돌아가시겠습니까?");
